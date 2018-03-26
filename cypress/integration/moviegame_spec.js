329ea7dbb045548f48037f3c3a517f4d947aba42
ayshiff <remi.d45@gmail.com>
2018-03-26 09:33:14 +0200

    Update Cypress#2

---
+++ cypress/integration/moviegame_spec.js
@@ -0,0 +1,17 @@
+describe("Base application", () => {
+
+    it("clicks the button '' ", () => {
+        cy.visit("http://localhost:3000");
+        cy.get(".search").click();
+      });
+
+    it("clicks some movies...", () => {
+        cy.wait(3000);
+        cy.get('.Image').click();
+  
+        cy.wait(2000);
+        cy.get('.Image2').click();
+    })
+
+    
+})
