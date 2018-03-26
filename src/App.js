c5fbed055f5e0423f3f78501ec2ef9639b06b4ea
ayshiff <remi.d45@gmail.com>
2018-03-26 11:19:38 +0200

    Update graphQL

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ import MenuItem from 'material-ui/MenuItem';
 import { setTimeout } from 'timers';
 import Modal from 'react-modal';
 
+// Include Apollo
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
 const customStyles = {
   content : {
     top                   : '50%',
@@ -32,15 +36,13 @@ const customStyles = {
 Modal.setAppElement("#root")
 
 
-// Include Apollo
-import ApolloClient from "apollo-boost";
-import gql from "graphql-tag";
+
 
 // needs to implement dynamic uri
-const client = new ApolloClient({
+/* const client = new ApolloClient({
   uri: "https://api.themoviedb.org/3/discover/movie?api_key=6411cdf86812f3aa034a00c5987adb76&language=fr-FR&include_adult=false&include_video=false&page="
 });
-
+*/
 
 
 
@@ -91,6 +93,13 @@ class App extends Component {
     var pagevalue = Math.floor(Math.random() * 100) + 1;
     
     var reex = /a-z!?,:0-9/
+
+    /* Apollo client */
+
+    /* const client = new ApolloClient({
+      uri: "https://api.themoviedb.org/3/discover/movie?api_key=6411cdf86812f3aa034a00c5987adb76&language=fr-FR&include_adult=false&include_video=false&page=" + pagevalue
+    }); */
+
     
         // First request
         axios.get(this.state.url + pagevalue)
